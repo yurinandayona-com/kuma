e082968a94c3d1a2fa5684937e4c351fbdf744aa
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-12-04 03:12:28 +0900

    Add '--log-level' global option

--- cmd.go
+++ cmd.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"github.com/comail/colog"
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
 	"github.com/yurinandayona-com/kuma/cmd/connect"
 	"github.com/yurinandayona-com/kuma/cmd/serve"
 	"github.com/yurinandayona-com/kuma/cmd/token"
 	"github.com/yurinandayona-com/kuma/version"
+	"log"
 )
 
 var Cmd *cobra.Command
 
 func init() {
+	var logLevel string
+
 	Cmd = &cobra.Command{
 		Use:     "kuma",
 		Short:   "kuma: HTTP Tunnel over gRPC",
@@ -21,7 +25,20 @@ func init() {
 		},
 	}
 
+	Cmd.PersistentFlags().StringVarP(&logLevel, "log-level", "l", "info", "minimal log level")
+
 	Cmd.AddCommand(connect.Cmd)
 	Cmd.AddCommand(serve.Cmd)
 	Cmd.AddCommand(token.Cmd)
+
+	cobra.OnInitialize(func() {
+		colog.Register()
+
+		level, err := colog.ParseLevel(logLevel)
+		if err != nil {
+			log.Fatalf("alert: %s", err)
+		}
+
+		colog.SetMinLevel(level)
+	})
 }
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"github.com/comail/colog"
 	"os"
 )
-
-func init() {
-	colog.Register()
-}
 
 func main() {
 	if err := Cmd.Execute(); err != nil {
