c6824f5632ee7f0fecf1e6285cccd045232f321e
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-12-05 11:43:50 +0900

    Add more document comment

--- config/config.go
+++ config/config.go
@@ -1,2 +1,4 @@
 // Package config is utility collections about configuration file.
+//
+// It is the shallow wrapper for github.com/spf13/viper and gopkg.in/go-playground/validator.v9.
 package config
