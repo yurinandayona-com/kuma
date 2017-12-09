05fea42dbc1586cdf2619e2df9691ef55fd2fc26
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-12-09 23:30:54 +0900

    Run all prof server setup process in go routine

--- cmd/kuma/prof.go
+++ cmd/kuma/prof.go
@@ -11,13 +11,13 @@ import (
 )
 
 func init() {
-	port, err := freeport.GetFreePort()
-	if err != nil {
-		log.Printf("info: failed to get free port for pprof server: %s", err)
-	}
-
-	log.Printf("info: start pprof server: http://localhost:%d", port)
 	go func() {
+		port, err := freeport.GetFreePort()
+		if err != nil {
+			log.Printf("info: failed to get free port for pprof server: %s", err)
+		}
+
+		log.Printf("info: start pprof server: http://localhost:%d", port)
 		log.Printf("info: stop pprof server: %s", http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 	}()
 }
