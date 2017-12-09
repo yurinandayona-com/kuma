bb5b1870a15e01003361bbb29debdb286120570e
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-12-10 00:05:01 +0900

    Refactor pprof server start process

--- cmd/kuma/prof.go
+++ cmd/kuma/prof.go
@@ -12,13 +12,15 @@ import (
 )
 
 func init() {
-	go func() {
-		port, err := freeport.GetFreePort()
-		if err != nil {
-			log.Printf("info: failed to get free port for pprof server: %s", err)
-		}
+	go startPProfServer()
+}
 
-		log.Printf("info: start pprof server: http://localhost:%d", port)
-		log.Printf("info: stop pprof server: %s", http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
-	}()
+func startPProfServer() {
+	port, err := freeport.GetFreePort()
+	if err != nil {
+		log.Printf("info: failed to get free port for pprof server: %s", err)
+	}
+
+	log.Printf("info: start pprof server: http://localhost:%d", port)
+	log.Printf("info: stop pprof server: %s", http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
